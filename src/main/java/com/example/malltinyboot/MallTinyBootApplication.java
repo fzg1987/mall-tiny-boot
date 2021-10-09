194072e6d1fb33966f4dc6e3f9203adf7b76ad02
fangzongguang <zongguang.fang@bjrxtd.com>
2021-10-09 16:25:17 +0800

    New Features edit

--- src/main/java/com/example/malltinyboot/MallTinyBootApplication.java
+++ src/main/java/com/example/malltinyboot/MallTinyBootApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 开启组件扫描和应用装配
 
+/**
+ * @author fzg1987
+ */
 @SpringBootApplication
 public class MallTinyBootApplication {
 
     public static void main(String[] args) {
+        // 负责引导应用程序启动
         SpringApplication.run(MallTinyBootApplication.class, args);
     }
 
--- src/test/java/com/example/malltinyboot/MallTinyBootApplicationTests.java
+++ src/test/java/com/example/malltinyboot/MallTinyBootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.malltinyboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 class MallTinyBootApplicationTests {
 
+    @Autowired
+    private PmsBrandService pmsBrandService;
+
     @Test
-    void contextLoads() {
+    public void testMethod() {
+        List <PmsBrand> brandList = pmsBrandService.listAllBrand();
+        log.info("testMethod:{}",brandList);
     }
 
 }
