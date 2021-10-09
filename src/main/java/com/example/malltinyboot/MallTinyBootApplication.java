0886bfdd9c31e4c269d4f3f468a29fa8d0f77dc3
fangzongguang <zongguang.fang@bjrxtd.com>
2021-10-09 16:08:04 +0800

    Initial commit

---
+++ src/main/java/com/example/malltinyboot/MallTinyBootApplication.java
@@ -0,0 +1,15 @@
+package com.example.malltinyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 开启组件扫描和应用装配
+
+@SpringBootApplication
+public class MallTinyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyBootApplication.class, args);
+    }
+
+}
